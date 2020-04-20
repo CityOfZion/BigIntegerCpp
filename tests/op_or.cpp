63cfd4a29c8c55f99d7ec62e687a97b7addc4be3
Erik van den Brink <git@erikvandenbrink.com>
2020-04-20 14:38:54 +0200

    - add op_xor tests

---
+++ tests/op_or.cpp
@@ -0,0 +1,138 @@
+#include "../src/BigInteger.h"
+#include "gtest/gtest.h"
+#include "Random.h"
+#include "stack_calc.h"
+#include "utils.h"
+
+static void VerifyOrString(std::string opstring)
+{
+    StackCalc sc(opstring);
+    while (sc.DoNextOperation())
+    {
+        ASSERT_EQ(sc.snCalc.top().ToString(), sc.myCalc.top().ToString());
+    }
+}
+
+static byte_array GetRandomByteArray(Random random, int size)
+{
+    return MyBigIntImp::GetRandomByteArray(random, size);
+}
+
+static byte_array GetRandomByteArray(Random random)
+{
+    return GetRandomByteArray(random, random.Next(0, 1024));
+}
+
+
+static std::string Print(byte_array bytes)
+{
+    return MyBigIntImp::Print(bytes);
+}
+
+TEST(operator_tests, op_or) {
+    int s_samples = 10;
+    Random s_random(100);
+
+    byte_array tempByteArray1;
+    byte_array tempByteArray2;
+
+    // Or Method - Two Large BigIntegers
+    for (int i = 0; i < s_samples; i++)
+    {
+        tempByteArray1 = GetRandomByteArray(s_random);
+        tempByteArray2 = GetRandomByteArray(s_random);
+        VerifyOrString(Print(tempByteArray1) + Print(tempByteArray2) + "b|");
+    }
+
+    // Or Method - Two Small BigIntegers
+    for (int i = 0; i < s_samples; i++)
+    {
+        tempByteArray1 = GetRandomByteArray(s_random, 2);
+        tempByteArray2 = GetRandomByteArray(s_random, 2);
+        VerifyOrString(Print(tempByteArray1) + Print(tempByteArray2) + "b|");
+    }
+
+    // Or Method - One large and one small BigIntegers
+    for (int i = 0; i < s_samples; i++)
+    {
+        tempByteArray1 = GetRandomByteArray(s_random);
+        tempByteArray2 = GetRandomByteArray(s_random, 2);
+        VerifyOrString(Print(tempByteArray1) + Print(tempByteArray2) + "b|");
+
+        tempByteArray1 = GetRandomByteArray(s_random, 2);
+        tempByteArray2 = GetRandomByteArray(s_random);
+        VerifyOrString(Print(tempByteArray1) + Print(tempByteArray2) + "b|");
+    }
+
+    // Or Method - One large BigIntegers and zero
+    for (int i = 0; i < s_samples; i++)
+    {
+        tempByteArray1 = GetRandomByteArray(s_random);
+        tempByteArray2 = byte_array{ 0 };
+        VerifyOrString(Print(tempByteArray1) + Print(tempByteArray2) + "b|");
+
+        tempByteArray1 = byte_array{ 0 };
+        tempByteArray2 = GetRandomByteArray(s_random);
+        VerifyOrString(Print(tempByteArray1) + Print(tempByteArray2) + "b|");
+    }
+
+    // Or Method - One small BigIntegers and zero
+    for (int i = 0; i < s_samples; i++)
+    {
+        tempByteArray1 = GetRandomByteArray(s_random, 2);
+        tempByteArray2 = byte_array{ 0 };
+        VerifyOrString(Print(tempByteArray1) + Print(tempByteArray2) + "b|");
+
+        tempByteArray1 = byte_array{ 0 };
+        tempByteArray2 = GetRandomByteArray(s_random, 2);
+        VerifyOrString(Print(tempByteArray1) + Print(tempByteArray2) + "b|");
+    }
+
+    // Or Method - One large BigIntegers and -1
+    for (int i = 0; i < s_samples; i++)
+    {
+        tempByteArray1 = GetRandomByteArray(s_random);
+        tempByteArray2 = byte_array{ 0xFF };
+        VerifyOrString(Print(tempByteArray1) + Print(tempByteArray2) + "b|");
+
+        tempByteArray1 = byte_array{ 0xFF };
+        tempByteArray2 = GetRandomByteArray(s_random);
+        VerifyOrString(Print(tempByteArray1) + Print(tempByteArray2) + "b|");
+    }
+
+    // Or Method - One small BigIntegers and -1
+    for (int i = 0; i < s_samples; i++)
+    {
+        tempByteArray1 = GetRandomByteArray(s_random, 2);
+        tempByteArray2 = byte_array{ 0xFF };
+        VerifyOrString(Print(tempByteArray1) + Print(tempByteArray2) + "b|");
+
+        tempByteArray1 = byte_array{ 0xFF };
+        tempByteArray2 = GetRandomByteArray(s_random, 2);
+        VerifyOrString(Print(tempByteArray1) + Print(tempByteArray2) + "b|");
+    }
+
+    // Or Method - One large BigIntegers and Int.MaxValue+1
+    for (int i = 0; i < s_samples; i++)
+    {
+        tempByteArray1 = GetRandomByteArray(s_random);
+        tempByteArray2 = byte_array{ 0x00, 0x00, 0x00, 0x80, 0x00 };
+        VerifyOrString(Print(tempByteArray1) + Print(tempByteArray2) + "b|");
+
+        tempByteArray1 = byte_array{ 0x00, 0x00, 0x00, 0x80, 0x00 };
+        tempByteArray2 = GetRandomByteArray(s_random);
+        VerifyOrString(Print(tempByteArray1) + Print(tempByteArray2) + "b|");
+    }
+
+    // Or Method - One small BigIntegers and Int.MaxValue+1
+    for (int i = 0; i < s_samples; i++)
+    {
+        tempByteArray1 = GetRandomByteArray(s_random, 2);
+        tempByteArray2 = byte_array{ 0x00, 0x00, 0x00, 0x80, 0x00 };
+        VerifyOrString(Print(tempByteArray1) + Print(tempByteArray2) + "b|");
+
+        tempByteArray1 = byte_array{ 0x00, 0x00, 0x00, 0x80, 0x00 };
+        tempByteArray2 = GetRandomByteArray(s_random, 2);
+        VerifyOrString(Print(tempByteArray1) + Print(tempByteArray2) + "b|");
+    }
+}
--- tests/scratch_test.cpp
+++ tests/scratch_test.cpp
@@ -4,6 +4,9 @@
 
 TEST(scratch1, scratch_tests)
 {
+    int tmp_int = 1234;
+    std::vector<unsigned char> v;
+    memcpy(&v[0], &tmp_int, 4);
     std::mt19937 bla(123);
     auto val = BigInteger(123);
     auto exp = BigInteger(3);
